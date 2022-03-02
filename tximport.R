a163eba76dff03a6f5223103084d2bce0c18afde
Charles Hawkins <charles.l.hawkins@wsu.edu>
2022-03-01 17:40:58 -0800

    tximport

--- tximport.R
+++ tximport.R
@@ -1,5 +1,6 @@
 # tximport salmon TMP using ZhongmuNo1
 rm(list = ls())
+BiocManager::install("apeglm")
 
 library(WGCNA)
 library(tximport)
@@ -10,6 +11,7 @@ library(csaw)
 library(tidyr)
 library(tidyverse)
 library(data.table)
+library(apeglm)
 
 #################
 list_2 <- c("LID50568_1001", "LID50568_1006", "LID50568_1020",
@@ -34,15 +36,47 @@ list_4 <- c('Saranac-DS-Leaf','Saranac-DS-Stem','Saranac-DS-Root',
 #################
 setwd("/home/hawkins/Documents/Cesar/RNA/globus/lordec_reports/rna_seq/rna_seq_fastq/shen_sf")
 data_rna_seq1 = list.files(pattern = "quant.sf", full.names = T)
+
 data_rna_seq2 <- c("./LID50570_1005_quant.sf", "./LID50570_1005_quant.sf", "./LID50570_1008_quant.sf",
                    "./LID50570_1012_quant.sf", "./LID50571_1019_quant.sf", "./LID50571_1018_quant.sf",
                    "./LID50571_1023_quant.sf", "./LID50572_1001_quant.sf", "./LID50574_1008_quant.sf",
                    "./LID50574_1012_quant.sf", "./LID50574_1018_quant.sf", "./LID50575_1019_quant.sf")
 
+# wilson
+data_rna_seq3 <- c("./LID50571_1023_quant.sf", "./LID50572_1001_quant.sf", "./LID50574_1008_quant.sf",
+                   "./LID50574_1012_quant.sf", "./LID50574_1018_quant.sf", "./LID50575_1019_quant.sf")
+
+# sanarac DS
+data_rna_seq4 <- c("./LID50570_1005_quant.sf", "./LID50570_1005_quant.sf", "./LID50570_1008_quant.sf",
+                   "./LID50570_1012_quant.sf", "./LID50571_1019_quant.sf", "./LID50571_1018_quant.sf")
+
 txi.1 <- tximport(data_rna_seq1, type="salmon", txOut=TRUE, countsFromAbundance="scaledTPM")
 txi.2 <- tximport(data_rna_seq2, type="salmon", txOut=TRUE, countsFromAbundance="scaledTPM")
-cts <- txi.2$counts
+txi.3 <- tximport(data_rna_seq3, type="salmon", txOut=TRUE, countsFromAbundance="scaledTPM")
+txi.4 <- tximport(data_rna_seq4, type="salmon", txOut=TRUE, countsFromAbundance="scaledTPM")
+
+cts <- txi.1$counts
 cts[1:5,1:5]
+dim(cts)
+#############
+cpms.2 <- as.data.frame(cts)
+cpms.2 <- tibble::rownames_to_column(cpms.2, "lid")
+cpms.2[1:5,1:5]
+cpms.3 <- separate(cpms.2, 1, c('gene_id', 'rest'), sep = ";", remove = TRUE, convert = FALSE, extra = "warn")
+cpms.3[1:5,1:5]
+cpms.3 <- cpms.3[,-2]
+cpms.3.1 <- cpms.3 %>% remove_rownames() %>% column_to_rownames(var = 'gene_id')
+cpms.3.1[1:5,1:5]
+colnames(cpms.3.1) <- list_4
+dim(cpms.3.1)
+
+cpms.3.2 <- tibble::rownames_to_column(cpms.3.1, "lid") %>% separate(1, c('gene_id', 'isoform'), sep = "\\.", remove = FALSE, convert = FALSE, extra = "warn")
+cpms.3.2[1:5,1:5]
+dim(cpms.3.2)
+save(cpms.3.2, file = "~/Documents/Cesar/RNA/globus/lordec_reports/rna_seq/rna_seq_fastq/shen_sf/cpms.3.2.RData")
+
+#############
+
 normMat <- txi.1$length
 normMat <- normMat/exp(rowMeans(log(normMat)))
 normCts <- cts/normMat
@@ -69,8 +103,6 @@ cpms[1:5,1:5]
 colnames(cpms) <- list_3
 thres <- cpms > 0.5
 keep <- rowSums(thres) >= 2 
-
-
 
 cpms.2 <- as.data.frame(cpms)
 cpms.2 <- tibble::rownames_to_column(cpms.2, "lid")
@@ -109,12 +141,6 @@ dim(cpms.1)
 ######################
 # DegSeq2
 
-samples
-samples_1 <- unite(samples, col = condition_1, variety, condition, sep = "_") 
-
-sampleTable <- data.frame(condition = factor(rep(c("A", "B"), each = 3)))
-rownames(sampleTable) <- colnames(txi$counts)
-
 setwd("~/Documents/Cesar/RNA/globus/lordec_reports/rna_seq/salmon_sf")
 data_rna_seq1 = list.files(pattern = "quant.sf", full.names = T)
 
@@ -123,17 +149,12 @@ txi.1 <- tximport(data_rna_seq1, type="salmon", txOut=TRUE,
 txi.1$counts
 
 #all samples
-samples <- read.csv('~/Documents/Cesar/RNA/globus/lordec_reports/wgcna/sample_metadata2.csv')
-samples1 <- samples %>% remove_rownames() %>% column_to_rownames(var = 'sample_id')
-head(samples1)
-# head(datTraits)
-# datTraits1 <- datTraits[,-c(1:9)]
-# head(datTraits1)
-str(samples1)
-list_4 <- colnames(samples1)
-samples1[,list_4] <- lapply(samples1[,list_4], factor)
+samples <- read.csv('~/Documents/Cesar/RNA/globus/lordec_reports/wgcna/sample_metadata3.csv')
+str(samples)
+list_4 <- colnames(samples)
+samples[,list_4] <- lapply(samples[,list_4], factor)
 
-dds <- DESeqDataSetFromTximport(txi.2, samples1, ~ variety + condition)
+dds <- DESeqDataSetFromTximport(txi.3, samples, ~ condition)
 
 #pre-filtering
 keep <- rowSums(counts(dds)) >= 10
@@ -142,54 +163,37 @@ dds <- dds[keep,]
 # Differential expression analysis
 dds <- DESeq(dds)
 res <- results(dds)
-summary(res)
+plotCounts(dds, gene = which.min(res$padj), intgroup = "condition")
 
-# save.image("~/Documents/Cesar/RNA/globus/lordec_reports/rna_seq/rna_seq_fastq/shen_sf/tximport.RData")
+summary(res)
+sum(res$padj < 0.1, na.rm = T)
+resultsNames(dds)
+resOrdered <- res[order(res$pvalue), ]
+resSig <- subset(resOrdered, padj < 0.1)
+resSig.1 <- as.data.frame(resSig)
+
+
+# log fold change shrinkage for visualization and ranking
+resLFC <- lfcShrink(dds, coef = "condition_DS_vs_CK", type = "apeglm")
+summary(resLFC)
+plotMA(resLFC, ylim = c(-2,2))
 
 dds.1 <- varianceStabilizingTransformation(dds, blind = TRUE, fitType = "parametric")
 dds.1@assays
 resultsNames(dds)
 
-# Remove all rows with less than n counts across all samples, where n=#samples
-raw_counts <- txi.1$counts
-dim(raw_counts)
-raw_counts1 <- t(raw_counts)
-low_count_mask <- rowSums(raw_counts) < ncol(raw_counts)
-dim(low_count_mask)
-sprintf("Removing %d low-count genes (%d remaining).", sum(low_count_mask), 
-        sum(!low_count_mask))
-
-log_counts <- log2(raw_counts + 1)
-sft = pickSoftThreshold (cpms.1, powerVector = powers, verbose = 5)
-
-log_counts[1:5, 1:21]
-dim(log_counts)
-gsg = goodSamplesGenes(log_counts, verbose = 3);
-gsg$allOK
-gsg$goodSamples
-
-options(stringsAsFactors = FALSE);
-powers = c(c(1:10), seq(from = 12, to=20, by=2))
-sft = pickSoftThreshold(log_counts, powerVector = powers, verbose = 5)
-
-sizeGrWindow(9,5);
-par(mfrow= c(1,2));
-cex1 = 0.9;
-
-plot(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2], xlab = "Soft Threshold (power)",ylab="Scale Free Topology Model Fit,signed R^2",type="n", main = paste("Scale independence"));
-text(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],labels=powers,cex=cex1,col="red");
-abline(h=0.90, cool="red")
-
-plot(sft$fitIndices[,1], sft$fitIndices[,5], xlab="Soft Threshold (power)",ylab="Mean Connectivity", type="n", main = paste("Mean connectivity"))
-text(sft$fitIndices[,1], sft$fitIndices[,5], labels=powers, cex=cex1,col="red")
-
 ######################
+
+### Extract normalized expression for significant genes from the OE and control samples (4:9), and set the gene column (1) to row names
+norm_OEsig <- normalized_counts[,c(1,4:9)] %>% 
+  filter(gene %in% sigOE$gene) %>% 
+  data.frame() %>%
+  column_to_rownames(var = "gene") 
+
 
 head(cpms.5)
 library(pheatmap)
 pheatmap(cpms.5)
-
-
 
 cal_z_score <- function(x){
   (x - mean(x)) / sd(x)
@@ -204,15 +208,82 @@ pheatmap(data_subset_norm, drop_levels = TRUE,
 
 data_subset_norm[1:4, 1:4]
 
-pathways <- read.csv("modules_pathways.csv", header = T)
+##################
+# edgeR
+library(edgeR)
 
-head(pathways)
-dim(pathways)
-colnames(pathways)
-pathways.1 <- separate(pathways, 12, c('Pathways', 'ID'), sep = "\\.", remove = TRUE, convert = FALSE, extra = "merge")
-head(pathways.1)
-colnames(pathways.1)
-pathways.2 <- separate(pathways.1, 12, c('Pathways_1', 'Pathways_2', 'Pathways_3'), sep = ";", remove = TRUE, convert = FALSE, extra = "warn")
-cc1 <- count(pathways.2, Pathways_1, Pathways_2)
-cc <- count(pathways.2, module, Pathways_1, Pathways_2)
-cc2 <- count(pathways.2, treatment, Pathways_1, Pathways_2)
+samples <- read.csv('~/Documents/Cesar/RNA/globus/lordec_reports/wgcna/sample_metadata3.csv')
+str(samples)
+list_4 <- colnames(samples)
+samples[,list_4] <- lapply(samples[,list_4], factor)
+
+
+cts <- txi.3$counts
+normMat <- txi.3$length
+
+# Obtaining per-observation scaling factors for length, adjusted to avoid
+# changing the magnitude of the counts.
+normMat <- normMat/exp(rowMeans(log(normMat)))
+normCts <- cts/normMat
+
+# Computing effective library sizes from scaled counts, to account for
+# composition biases between samples.
+eff.lib <- calcNormFactors(normCts) * colSums(normCts)
+
+# Combining effective library sizes with the length factors, and calculating
+# offsets for a log-link GLM.
+normMat <- sweep(normMat, 2, eff.lib, "*")
+normMat <- log(normMat)
+
+# Creating a DGEList object for use in edgeR.
+y <- DGEList(cts, group = samples$condition)
+y <- scaleOffset(y, normMat)
+# filtering
+keep <- filterByExpr(y)
+table(keep)
+y <- y[keep, ]
+# y is now ready for estimate dispersion functions see edgeR User's Guide
+dim(y)
+
+# CPMs
+# cpms <- edgeR::cpm(y, offset = y$offset, log = FALSE)
+
+y <- calcNormFactors(y)
+head(y$samples)
+
+design <- model.matrix(~samples$condition)
+y <- estimateDisp(y, design, robust = T)
+plotBCV(y)
+fit <- glmQLFit(y, design, robust=TRUE)
+plotQLDisp(fit)
+qlf <- glmQLFTest(fit)
+topTags(qlf, n = 15)
+summary(decideTests(qlf))
+
+et <- exactTest(y, pair = c("CK", "DS"))
+
+topTags(et)
+?glmQLFTest
+
+
+
+
+
+
+top <- rownames(topTags(qlf))
+cpm(y)[top,]
+
+
+plotMD(qlf)
+abline(h=c(-1,1), col="blue")
+
+FDR <- p.adjust(qlf$table$PValue, method="BH")
+sum(FDR < 0.05)
+
+y1 <- estimateGLMCommonDisp(y, method = "deviance", robust = TRUE, subset = NULL)
+
+qlf <- glmQLFTest(fit, coef=2:3)
+
+mod <- model.matrix(~0 + samples$condition)
+contrast.mat <- makeContrasts(Diff = CK - DS, levels = mod)
+
